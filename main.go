7c74f8bd7c931008465f8f1e7b8f79d604e3c4c5
Yedija Johanan Siregar <115084766+YedijaJS@users.noreply.github.com>
2025-09-18 13:43:31 +0700

    Yedija Johanan Siregar

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Selamat Datang di Telyu")
+	fmt.Println("Selamat datang di telyuu")
 	nama := ""
 	fmt.Print("Masukan nama anda: ")
 	fmt.Scan(&nama)
