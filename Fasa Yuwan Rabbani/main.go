86af1162d6dd6c3c7bcadc2b8612e2ceb53e25a2
FasaYuwanRabbani <finershark@gmail.com>
2025-09-18 11:51:22 +0800

    Add files via upload

---
+++ Fasa Yuwan Rabbani/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Selamat datang di Telyu")
+	nama := ""
+	fmt.Print("Masukan Nama Njenengan")
+	fmt.Scan(&nama)
+	fmt.Printf("halo, %s", nama)
+}
