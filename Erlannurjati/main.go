ce4ec0c5449441448546cea2802ef093c7ef9b77
erlan19 <suparlansmile2019@gmail.com>
2025-09-18 15:09:20 +0700

    Add files via upload

---
+++ Erlannurjati/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("-OMAAKK WELCOME TO TELYU-")
+	nama := ""
+	fmt.Print("Masukin asmane panjenengan: ")
+	fmt.Scan(&nama)
+	fmt.Print("Halo, %s", nama)
+}
