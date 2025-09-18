27a09bae097c67d2dc455a1c1df08bc768542b68
samaisyamsi123-alt <samaisyamsi123@gmail.com>
2025-09-18 10:38:34 +0700

    Add files via upload

---
+++ hafizd sama'i/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Selamat Datang di Telyu")
+	name := ""
+	fmt.Print("Hafizd Samai Syamsi")
+	fmt.Scan(&name)
+	fmt.Printf("hallo, %s", name)
+}
